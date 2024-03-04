befacc68673edb6295c2721bc0d89f30d8a97ad9
Aaron Graves <ajgraves@ajgraves.com>
2024-03-04 10:30:35 -0600

    Initial Commit

---
+++ iGoTheDistance/iGoTheDistanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  iGoTheDistanceApp.swift
+//  iGoTheDistance
+//
+//  Created by Aaron Graves on 3/4/24.
+//
+
+import SwiftUI
+
+@main
+struct iGoTheDistanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
